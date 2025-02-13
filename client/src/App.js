385d75a86055393e44decddaf308bbb34b231644
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-13 11:59:12 +0100

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
+// Import React Route
 import {Routes, Route, Link} from "react-router-dom";
+
+// Import Css Filies
+import "./css_styles/index.css";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-        <Link to="/button">
-          <button>Click To Button</button>
+        <Link to="/contact">
+            <button className="bg-red-200 p-5 m-5">go to contact</button>
         </Link>
+
         <Routes>
-          <Route path="/button" element={<h1>Hello Button</h1>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}>
+
+          </Route>
+          <Route path="/" element={
+            <>
+                <Home />
+                <Home></Home>
+            </>
+          }>
+          </Route>
         </Routes>
     </div>
   );
---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+export default function Contact() {
+  return (
+
+    <div>
+        <Link to="/">
+            <button className='bg-yellow-200 p-5'>Go to Home</button>
+        </Link>
+    </div>
+  )
+}
